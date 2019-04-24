71291c5a2673076c48d438b18b045364cfbab613
sarthak agarwal <sarthak.agarwal97@gmail.com>
2019-04-25 02:45:50 +0530

    second commit

--- App.js
+++ App.js
@@ -10,61 +10,42 @@ import {
 
 export default class App extends Component{
 
-  state ={
-    value:"username",
-    value2: "password"
-  }
-
-  handleChangeText(newvalue , passvalue){
-    this.setState({
-      value :newvalue,
-      value:passvalue
-      
-    });
-  }
   render(){
     return(
-      <View styles={styles.main}>
-      <View >
-      <Text>Username </Text>
-      <TextInput onChange={this.handleChangeText.bind(this)} defaultValue={this.state.value}></TextInput>
-      </View>
-      <View >
-      <Text>Password </Text>
-      <TextInput onChange={this.handleChangeText.bind(this)} defaultValue={this.state.value2}></TextInput>
-      
-      </View>
-      <Button title={"hello"}></Button>
+      <View style={styles.container}>
+            <View style={styles.result}></View>
+            <View style={styles.calculation}></View>
+            <View style={styles.buttons}>
+               <View style={styles.numbers}></View>
+               <View style={styles.operations}></View>
+            </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  main:{
-    flex:1,
-    backgroundColor: 'yellow',
-    justifyContent:'center'
-  },
-  text:{
-    fontSize: 20
-  },
-  inner11:{
-    flex:1,
-    flexDirection:'row',
-    backgroundColor: 'green'
-  },
-  inner12:{
-    flex:2,
-    flexDirection:'row',
-    backgroundColor: 'yellow'
-  },inner121:{
-    flex:2,
-    flexDirection:'column',
-    backgroundColor: 'orange'
-  },inner122:{
-    flex:3,
-    flexDirection:'column',
-    backgroundColor: 'black'
-  }
+container:{
+  flex:1,
+  backgroundColor:'red'
+},
+result:{
+  flex:2,
+  backgroundColor:'yellow'
+},calculation:{
+  flex:1,
+  backgroundColor:'green'
+},
+buttons:{
+  flexGrow:7,
+  flexDirection:'row'
+},
+numbers:{
+  flex:3,
+  backgroundColor:'blue'
+},
+operations:{
+  flex:1,
+  backgroundColor:'white'
+}
 })
